b04fe0b43b65173476836fbf75c232fcec2c7366
Brandon Elzy <Brandon Elzy>
2017-05-19 17:39:48 -0500

    first commit

---
+++ mailto.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title></title>
+		<meta charset="UTF-8" />
+	</head>
+	
+	<body>
+		<?php
+			if (isset($_POST['submit']))
+			{
+				echo $_POST['name'];
+			}
+		?>
+	</body>
+</html>
